21c797ad21996cd2f7e9a017ec479d98cf7ff503
Donal Burns <donal.w.burns@hotmail.com>
2019-11-18 19:20:48 +0000

    Worked on Vectorize2.R

--- Week3/Code/Vectorize2.R
+++ Week3/Code/Vectorize2.R
@@ -1,4 +1,4 @@
-## Desc: A challenge to reduce the runtime of a script which applies the Ricker model to some data.
+0## Desc: A challenge to reduce the runtime of a script which applies the Ricker model to some data.
 
 # Runs the stochastic (with gaussian fluctuations) Ricker Eqn .
 
@@ -17,7 +17,7 @@ stochrick<-function(p0=runif(1000,.5,1.5),r=1.2,K=1,sigma=0.2,numyears=100)
       N[yr,pop] <- N[yr-1,pop] * exp(r * (1-N[yr-1,pop]/K) + rnorm(1,0,sigma))
     }
   }
- return(N)
+  return(N)
 
 }
 
@@ -48,22 +48,3 @@ print(system.time(res1<-stochrick()))
 print("Vectorized Stochastic Ricker takes:")
 print(system.time(res2<-stochrickvect()))
 
-
-
-
-#######GRAVEYARD####
-
-# stochrickvect<-function(p0=runif(1000,.5,1.5),r=1.2,K=1,sigma=0.2,numyears=100)
-# {
-#   #initialize
-#   N<-matrix(NA,numyears,length(p0))
-#   N[1,]<-p0
-  
-#   for (pop in 1:length(p0)){ #loop through the populations
-#   # sapply(1:length(p0), function(pop))
-        
-#     # sapply(2:numyears, function(yr) N[yr,pop]<-N[yr-1,pop]*exp(r*(1-N[yr-1,pop]/K)+rnorm(1,0,sigma)))
-#   }
-#  return(N)
-  
-# }
