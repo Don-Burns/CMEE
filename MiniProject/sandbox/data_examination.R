1380a9094e167c922fdf336770186e618870f756
Donal Burns <donal.w.burns@hotmail.com>
2019-11-18 19:20:09 +0000

    Added data_examination.R

---
+++ MiniProject/sandbox/data_examination.R
@@ -0,0 +1,17 @@
+### Desc: a script to look at and do prelim visualisation of `../data/CRat.csv`.  Metadata can be found in `../data/BiotraitsTemplateDescription.pdf`
+
+# setwd("~/Documents/CMEECourseWork/MiniProject/sandbox/") # to be deleted, only for me to use 
+
+##########constants##############
+##Any constants or small changes for the model should be made here.  Do not modify below here unless the user is confident they know what they are doing
+###############################
+d <- read.csv("../data/CRat.csv")
+
+
+###########packages####################
+
+
+##############functions################
+
+
+###############main###############
